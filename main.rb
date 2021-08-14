2bf234e5c6d8a0ebe068cf81a598172366ba4b4e
Gabriel Morales <gabrielmorales@Gabriels-MacBook-Pro.local>
2021-08-14 10:56:25 -0400

    First attempt using chrome webdriver

---
+++ main.rb
@@ -0,0 +1,19 @@
+require "selenium-webdriver"
+
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--disable-translate')
+options.add_argument('--chromever=92.0.4515.131')
+driver = Selenium::WebDriver.for :chrome, options: options
+driver.navigate.to "http://google.com"
+
+element = driver.find_element(name: 'q')
+element.send_keys "Hello WebDriver!"
+element.submit
+
+puts driver.title
+
+driver.quit
+
+puts "HelloW"
