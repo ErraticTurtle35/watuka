82f1889075a8d5a489e11526a4a50a74c8180d61
Gabriel Morales <gabrielmorales@Gabriels-MacBook-Pro.local>
2021-08-16 09:52:47 -0400

    Add a specific version to ruby

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require "selenium-webdriver"
 
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--disable-popup-blocking')
-options.add_argument('--disable-translate')
-options.add_argument('--chromever=92.0.4515.131')
-driver = Selenium::WebDriver.for :chrome, options: options
+# options = Selenium::WebDriver::Chrome::Options.new
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument('--disable-popup-blocking')
+# options.add_argument('--disable-translate')
+# options.add_argument('--chromever=92.0.4515.131')
+# driver = Selenium::WebDriver.for :chrome, options: options
+
+# driver = Selenium::WebDriver.for :firefox
 driver.navigate.to "http://google.com"
 
 element = driver.find_element(name: 'q')
