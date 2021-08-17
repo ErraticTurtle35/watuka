982c04b80e40009ba47c393b2c45f8d99f6c052b
Gabriel Morales <gabrielmorales@Gabriels-MacBook-Pro.local>
2021-08-17 19:09:24 -0400

    Refactor names from talent profile page

--- pages/talent_profile_page.rb
+++ pages/talent_profile_page.rb
@@ -3,40 +3,70 @@ require_relative 'base_page'
 class TalentProfilePage < BasePage
   def initialize(driver = nil)
     super(driver)
-    @identify_name = 'div[contains(@class,  "freelancer-tile")]/div/div/div[contains(@class,  "cfe-ui-freelancer-tile-identity")]/div[@class="identity-content"]/button/div[@class="identity-name"]'
-    @freelancer_title = 'div[contains(@class,  "freelancer-tile")]/div/div/div[contains(@class,  "cfe-ui-freelancer-tile-identity")]/div[@class="identity-content"]/p'
-    @country_name = 'div[contains(@class, "freelancer-tile")]/div/div/div[contains(@class,  "cfe-ui-freelancer-tile-identity")]/div[@class="identity-content"]/div/span[@itemprop="country-name"]'
-    @profile_overview = 'div[contains(@class, "freelancer-tile")]/div/div[@class="mt-10"]/div[contains(@class, "up-line-clamp-v2-wrapper")]/div[contains(@class, "up-line-clamp-v2")]'
-    @skills = 'div[contains(@class,  "skills")]/div/div[@class="up-skill-wrapper"]/div[@class="up-skill-badge"]'
+    @profile_viewer_class = 'profile-viewer'
+    @profile_viewer_child = 'div/div/div/section/div/div/div'
+    @identity_content_class = 'identity-content'
+    @talent_name = 'div/h1'
+    @profile_section = 'div/div/div/section[@class="row"]'
+    @profile_section_container_div_class = 'min-width-0'
+    @profile_title = 'section/div/div/div/h2'
+    @country = 'div/span[contains(@itemprop, "country-name")]'
+    @profile_overview = 'section/div[@class="row"]/div[contains(@class,"up-line-clamp-v2")]'
   end
 
   def gather_talent
-    profile_viewer = self.get_element('profile-viewer', 'ClassName')
-    profile_viewer_child = self.get_child_element(profile_viewer, 'div/div/div/section/div/div/div', 'Xpath')
-    identity_content = self.get_child_element(profile_viewer_child, 'identity-content', 'ClassName')
-    talent_name = self.get_child_element(identity_content, 'div/h1', 'Xpath')
-
-    element1 = self.get_child_element(profile_viewer, 'div/div/div/section[@class="row"]', 'Xpath')
-    element2 = self.get_child_element(element1, 'min-width-0', 'ClassName')
-    element3 = self.get_child_element(element2, 'section/div/div/div/h2', 'Xpath')
-
-    element4 = self.get_child_element(identity_content, 'd-block', 'ClassName')
-    element5 = self.get_child_element(element4, 'div/span[contains(@itemprop, "country-name")]', 'Xpath')
-
-    element6 = self.get_child_element(element2, 'section/div[@class="row"]/div[contains(@class,"up-line-clamp-v2")]', 'Xpath')
-    element7 = self.get_child_element(element6, 'text-pre-line', 'ClassName')
-
+    talent_name = get_talent_name_element
+    profile_title = get_profile_title_element
+    country = get_country_element
+    profile_overview = get_profile_overview_element
     skills = []
-    self.get_child_elements(element2, 'section[last()]/div/ul/li', 'Xpath').each_with_index do |skill, skill_position|
+    get_skill_elements.each do |skill|
       skills.push(skill.text)
     end
     {
       'talent_name' => talent_name.text,
-      'profile_title' => element3.text,
-      'country' => element5.text,
-      'profile_overview' => element7.text,
+      'profile_title' => profile_title.text,
+      'country' => country.text,
+      'profile_overview' => profile_overview.text,
       'skills' => skills
     }
+  end
+
+  def get_skill_elements
+    profile_viewer = self.get_element(@profile_viewer_class, 'ClassName')
+    profile_section = get_child_element(profile_viewer, @profile_section, 'Xpath')
+    profile_section_container_div = get_child_element(profile_section, @profile_section_container_div_class, 'ClassName')
+    self.get_child_elements(profile_section_container_div, 'section[last()]/div/ul/li', 'Xpath')
+  end
+
+  def get_profile_overview_element
+    profile_viewer = self.get_element(@profile_viewer_class, 'ClassName')
+    profile_section = get_child_element(profile_viewer, @profile_section, 'Xpath')
+    profile_section_container_div = get_child_element(profile_section, @profile_section_container_div_class, 'ClassName')
+    profile_overview = self.get_child_element(profile_section_container_div, @profile_overview, 'Xpath')
+    self.get_child_element(profile_overview, 'text-pre-line', 'ClassName')
+  end
+
+  def get_country_element
+    profile_viewer = self.get_element(@profile_viewer_class, 'ClassName')
+    profile_viewer_child = self.get_child_element(profile_viewer, @profile_viewer_child, 'Xpath')
+    identity_content = self.get_child_element(profile_viewer_child, @identity_content_class, 'ClassName')
+    country_container = self.get_child_element(identity_content, 'd-block', 'ClassName')
+    self.get_child_element(country_container, @country, 'Xpath')
+  end
+
+  def get_profile_title_element
+    profile_viewer = self.get_element(@profile_viewer_class, 'ClassName')
+    profile_section = get_child_element(profile_viewer, @profile_section, 'Xpath')
+    profile_section_container_div = get_child_element(profile_section, @profile_section_container_div_class, 'ClassName')
+    get_child_element(profile_section_container_div, @profile_title, 'Xpath')
+  end
+
+  def get_talent_name_element
+    profile_viewer = self.get_element(@profile_viewer_class, 'ClassName')
+    profile_viewer_child = self.get_child_element(profile_viewer, @profile_viewer_child, 'Xpath')
+    identity_content = self.get_child_element(profile_viewer_child, @identity_content_class, 'ClassName')
+    self.get_child_element(identity_content, @talent_name, 'Xpath')
   end
 end
 
