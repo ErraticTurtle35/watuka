3d231c98c71b812945077fa8f88d78e757796978
Gabriel Morales <gabrielmorales@Gabriels-MacBook-Pro.local>
2021-08-17 09:25:46 -0400

    Add basic structure for manage the call of elements

--- main.rb
+++ main.rb
@@ -1,15 +1,30 @@
 require_relative 'utils/wakuta_selenium_driver_factory'
+require_relative 'pages/base_page'
 
-wakuta_selenium_driver = WakutaSeleniumDriverFactory.new
-driver = wakuta_selenium_driver.webdriver_instance
-driver.navigate.to "http://google.com"
+base_page = BasePage.new
+base_page.navigate_to "http://google.com"
 
-element = driver.find_element(name: 'q')
+element = base_page.get_element('q', 'Name')
+# element = base_page.driver.find_element(name: 'q')
 element.send_keys "Hello WebDriver!"
 element.submit
 
-puts driver.title
+puts base_page.driver.title
 
-driver.quit
+base_page.driver.quit
 
 puts "HelloW"
+
+# wakuta_selenium_driver = WakutaSeleniumDriverFactory.new
+# driver = wakuta_selenium_driver.webdriver_instance
+# driver.navigate.to "http://google.com"
+#
+# element = driver.find_element(name: 'q')
+# element.send_keys "Hello WebDriver!"
+# element.submit
+#
+# puts driver.title
+#
+# driver.quit
+#
+# puts "HelloW"
--- pages/base_page.rb
+++ pages/base_page.rb
@@ -7,4 +7,43 @@ class BasePage
     @driver = webdriver_instance
     super
   end
+
+  def navigate_to(url)
+    @driver.navigate.to url
+  end
+
+  def get_element(locator, locator_type = "Xpath")
+    begin
+      element_type = get_available_type_elements(locator_type)
+      return @driver.find_element(element_type, locator)
+    rescue
+      puts('A') # TODO: Clear the error messages
+    ensure
+      puts('A') # TODO: Clear the error messages
+    end
+  end
+
+  def get_available_type_elements(element_type)
+    case element_type
+    when 'ClassName'
+      :class
+    when 'CssSelector'
+      :css
+    when 'Id'
+      :id
+    when 'LinkText'
+      :link
+    when 'Name'
+      :name
+    when 'PartialLinkText'
+      :partial_link_text
+    when 'TagName'
+      :tag_name
+    when 'Xpath'
+      :xpath
+    else
+      puts('AAA') # TODO: Catch exception
+    end
+  end
+
 end
